c1a431e868649aee3a98fca9d190e6052ebdb9f8
darshgup139 <adarshgupta139@gmail.com>
2016-03-27 16:37:16 +0530

    1st Commit

---
+++ AddTPC_Beans/src/org/crce/interns/beans/UserBean.java
@@ -0,0 +1,22 @@
+package org.crce.interns.beans;
+
+import java.io.Serializable;
+
+public class UserBean implements Serializable {
+	private String username;
+	private String role_id;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getRole_id() {
+		return role_id;
+	}
+	public void setRole_id(String role_id) {
+		this.role_id = role_id;
+	}
+	
+}
